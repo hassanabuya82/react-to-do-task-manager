964dc6e3dee79f9946d31d7b45ef643ba973aae0
unknown <hassanabuya82@gmail.com>
2023-06-19 17:35:27 +0300

    updating

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -1,11 +1,36 @@
+import { useState } from 'react';
 import Header from './components/Header'
 import Tasks from './components/Tasks';
 
 function App() {
+
+  const [tasks, setTask] = useState(
+    [
+        {
+          "id": 1,
+          "text": "Doctors Appointment",
+          "day": "Feb 5th at 2:30pm",
+          "reminder": true
+        },
+        {
+          "id": 2,
+          "text": "Meeting at School",
+          "day": "Feb 6th at 1:30pm",
+          "reminder": true
+        },
+        {
+          "id": 3,
+          "text": "Do Something",
+          "day": "Feb 7th at 1:30pm",
+          "reminder": false
+        },
+]
+)
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
